70c0da5bd1850e3c68297fdb39ed5bc53f5a3a71
Heru Handika <herubiolog@gmail.com>
2022-10-11 12:19:53 -0500

    Match AMAS check aligned keywords.

--- src/parser.rs
+++ src/parser.rs
@@ -147,7 +147,7 @@ impl<'a> Parser<'a> {
                 apps.version = format!("v{}", version);
             }
             app if app.contains("AMAS") => {
-                if app.contains("aligned") {
+                if app.contains("Aligned") {
                     apps.name = String::from("AMAS (--check-align)");
                 } else {
                     apps.name = String::from("AMAS");
